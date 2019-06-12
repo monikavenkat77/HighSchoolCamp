20b2e81d71826609d804ae542bdff90219bb148a
Monika Venkat <monikavenkat724@gmail.com>
2019-06-12 11:44:29 -0500

    added fucntions practice

---
+++ functions_practice.py
@@ -0,0 +1,17 @@
+"""
+title: funtions_practice
+author: Monika
+date: 2019-06-12 11:19
+"""
+#first
+def age_calculator(year1, year2):
+    return year1 - year2
+
+print(age_calculator(2019, 2003))
+
+#averaging numbers
+def avg_numbers(x, y, z):
+    return (x + y + z)/3
+
+print(avg_numbers(80, 90, 100))
+
