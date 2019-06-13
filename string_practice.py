c35eb71430db5d9c39d792c46f092542b1296825
Monika Venkat <monikavenkat724@gmail.com>
2019-06-13 11:01:03 -0500

    added string practice labs

--- string_practice.py
+++ string_practice.py
@@ -4,20 +4,44 @@ author: Monika
 date: 2019-06-11 13:42
 """
 #FIRST ONE
-#answer = input("enter a character:")
-#print(answer in 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm')
+
+answer = input("enter a character:")
+print(answer in 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm')
+
 
 #SHORT HAND
 
-#short_hand = ("and = &" and "too = 2" and "you = U" and "for = 4")
+def short_hand (short):
+    short = short.replace("for", "4").replace("and", "&").replace("too", "2").replace("You", "U").replace("you", "U")
+    short = short.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+    return short
 
-#print(short_hand("Thank you for that! You are too sweet and kind!"))
+print(short_hand("How are you today?"))
 
-#credentialsgenerator
+#REMOVE CASE AND PUNCTUATION
 
-print(input("""first name:
-last name:
-city you were born in:
-university you graduated form:
-name of a relative:
-name of a friend:"""))
+def removing(check):
+    check = check.lower()
+    check = check.replace(" ","").replace(",", "").replace(":", "").replace("'","").replace(";","")
+    return check
+
+print(removing("Madam, I'm Adam"))
+
+#PALINDROME
+
+def palindrome(check):
+    check = removing(check)
+    return check == check[::-1]
+
+print(palindrome("Computer"))
+
+
+
+#CREDENTIALS GENERATOR
+
+#print(input("""first name:
+#last name:
+#city you were born in:
+#university you graduated form:
+#name of a relative:
+#name of a friend:"""))
